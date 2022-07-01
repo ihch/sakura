6c5140f1c091d4be88b146376680444b6da44464
ihch <nemu0sou@gmail.com>
2022-07-01 16:16:35 +0900

    feat: implement createElement function

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const text = "hello";
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      children,
+      ...props,
+    },
+  }
+}
 
-console.log(text);
+const div = createElement("div");
+const divWithChild = createElement("div", null, "a");
+const divWithChildren = createElement("div", null, "a", "b");
+
+console.log(div)
+console.log(divWithChild)
+console.log(divWithChildren)
