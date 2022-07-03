3b30c0d528a9700149a46c841789134256fa53fe
ihch <nemu0sou@gmail.com>
2022-07-03 19:02:33 +0900

    chore: react周りの依存を削除

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   esbuild: {
     jsxFactory: 'Ownact.createElement'
   },
-  plugins: [
-    react({
-      jsxRuntime: 'classic',
-    })
-  ],
 });
