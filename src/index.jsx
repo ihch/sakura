2f8597547434d58ae955f5da54219dbd6fe3cca7
ihch <nemu0sou@gmail.com>
2022-07-05 23:29:14 +0900

    fix: vite移行して必要なくなったpragmaを削除

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,6 @@ import Ownact from './ownact';
 
 const container = document.getElementById('root');
 
-/** @jsxRuntime classic */
-/** @jsx Ownact.createElement */
 const element = (
   <div id="foo">
     <a>bar</a>
